5beaa02c9fa6a1785c744b3f442dd18f257f1521
Muhammadumar <khilolamoon@gmail.com>
2023-07-25 22:28:30 +0500

    problems with logic

--- src/routes/workers.route.js
+++ src/routes/workers.route.js
@@ -9,6 +9,24 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
     const { id } = req.params;
     res.json(workersService.getWorkerById(id));
-})
+});
+
+router.post("/", (req, res) => {
+    const validatedData = workersService.validateCreateInput(req.body);
+    res.json(workersService.createWorker(validatedData));
+});
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+
+    const validatedData = workersService.validateUpdateInput(req.body);
+
+    res.json(workersService.updateWorker({ workerId: id, updateData: validatedData }));
+});
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    res.json(workersService.removeWorker(id));
+});
 
 module.exports = router;
--- src/service/workers.service.js
+++ src/service/workers.service.js
@@ -1,5 +1,5 @@
 const workers = require("../db/workers.db");
-
+const { v4: uuidv4 } = require('uuid');
 function getWorkers () {
     return workers
 }
@@ -19,7 +19,81 @@ function getWorkerById (id) {
     }
 }
 
+function createWorker (newWorker) {
+    workers.push(newWorker);
+    return {
+        status: "Success",
+        worker: newWorker
+    }
+}
+
+function updateWorker (params) {
+    const index = workers.findIndex((worker) => {
+        return worker.id === params.workerId
+    });
+
+    if(index === -1) throw new Error("Worker data is not found");
+
+    const updatedWorker = { ...workers[index], ...params.updateData};
+    workers[index] = updatedWorker;
+    return {
+        status: "Success",
+        worker: updatedWorker
+    }
+}
+
+const removeWorker = (id) => {
+    const index = workers.findIndex((worker) => {
+        return worker.id === id;
+    });
+
+    if(index === -1) {
+        throw new Error("Worker is not defined")
+    }
+
+    const deleteWr = workers.splice(index, 1);
+
+    return {
+        status: "Succes",
+        worker: deleteWr
+    }
+}
+
+const validateCreateInput = ({name, age, role, email}) => {
+    const newWorker = {};
+    newWorker.id = uuidv4();
+    if(!name) throw new Error('name field is not defined');
+    newWorker.name = name;
+    if(!age) throw new Error('age field is not defined');
+    newWorker.age = age;
+    if(!role) throw new Error('role field is not defined')
+    newWorker.role = role;
+    if(!email) throw new Error('email field is not defined');
+    newWorker.email = email;
+
+    return newWorker;
+}
+
+const validateUpdateInput = ({ name, age, role, email }) => {
+    const res = {};
+    if(name) res.name = name;
+    if(age) res.age = age;
+    if(role) res.role = role;
+    if(email) res.email = email;
+
+    if(Object.keys(res).length === 0) {
+        throw new Error("At least one field should be filled");
+    } else {
+        return res;
+    }
+}
+
 module.exports = {
     getWorkers,
-    getWorkerById
+    getWorkerById,
+    createWorker,
+    validateCreateInput,
+    updateWorker,
+    removeWorker,
+    validateUpdateInput
 }
